dc92dfb0436579fe674fde8f37ebc2c6613d0e50
unknown <suhasinigupta31@gmail.com>
2024-10-20 17:47:19 +0530

    Ticket Service completed

--- service/TicketService.java
+++ service/TicketService.java
@@ -1,11 +1,29 @@
 public class TicketService{
-    public TicketService(){
+    private GateRepository gaterepo ;
+    private ParkingLotRepository lotrepo ;
+    private TicketRepository ticketrepo ;
+    private ParkingSpotRepository spotrepo ;
 
+    public TicketService(GateRepository gaterepo,ParkingLotRepository lotrepo,TicketRepository ticketrepo,ParkingSpotRepository spotrepo){
+          this.gaterepo=gaterepo ;
+          this.lotrepo=lotrepo ;
+          this.spotrepo=spotrepo ;
+          this.ticketrepo=ticketrepo ;
     }
 
-    public Ticket generateTicket(){
+    public Ticket generateTicket(Vehicle vehicle, int gateId, int parkinglotId){
         SpotAllocationStrategy strategy= SpotAllocationStrategyFactory.getSpotAllocationStrategy() ;
         ParkingSpot spot=strategy.allocateSpot(parkinglot, vehicle) ;
-        return new Ticket(, LocalDateTime.now(),spot,vehicle, )
+        spot.setSpotStatus(ParkingSpotStatus.FULL) ;
+        spot.setVehicle(vehicle) ;
+        Gate gate=gaterepo.get(gateId) ;
+        ParkingLot lot=lotrepo.get(parkinglotId) ;
+        Ticket ticket=new Ticket() ;
+        ticket.setEntrygate(gate) ;
+        ticket.setVehicle(vehicle) ;
+        ticket.setOperator(gate.getOperator()) ;
+        ticket.setParkingSpot(spot) ;
+        ticket.setEntrytime(LocalDateTime.now()) ;
+        return ticket ;
     }
 }
